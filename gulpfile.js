8f1903977f95707af19c40eb9eec42cd8dc35737
Nikita Hovratov <nikita.h@live.de>
2018-05-10 19:45:57 +0200

    Add gulp and libraries

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+// node
+var extend = require('extend');
+
+// gulp
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var postcss = require('gulp-postcss');
+var sequence = require('gulp-sequence');
+var lec = require('gulp-line-ending-corrector');
+
+// -----
+// TASKS
+
+gulp.task('theme-css', function () {
+    return gulp.src('src/scss/theme.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([require('autoprefixer'), require('postcss-flexbugs-fixes')]))
+        .pipe(lec())
+        .pipe(gulp.dest('dist/css/'))
+        .pipe(cleanCSS())
+        .pipe(rename("theme.min.css"))
+        .pipe(gulp.dest('dist/css/'))
+});
+
+gulp.task('js', function () {
+    return gulp.src([
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/vue/dist/vue.min.js'
+    ])
+        .pipe(lec())
+        .pipe(gulp.dest('dist/js/libs'));
+});
+
+
+gulp.task('build', sequence(['js', 'theme-css']));
+
+gulp.task('watch', function () {
+    gulp.watch([
+        'src/scss/**/*.scss',
+        'src/scss/*.scss'
+    ], ['theme-css']);
+});
+
+gulp.task('default', ['watch']);
