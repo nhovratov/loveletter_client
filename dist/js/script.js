73d872b553a46d8f327abb3ca70daed761cccf0d
Nikita Hovratov <nikita.h@live.de>
2018-05-12 16:28:30 +0200

    Show number of connections

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,18 +1,21 @@
 var app = new Vue({
     el: "#app",
     data: {
-        connected: false,
-        cards: []
+        game: {
+            global: {},
+            local: {}
+        },
+        local: {}
     },
-    beforeMount: function () {
+    mounted: function () {
         window.conn = new WebSocket('ws://127.0.0.1:8080');
 
         conn.onopen = function (e) {
-            app.connected = true;
+            app.local.connected = true;
         };
 
         conn.onmessage = function (e) {
-            app.cards = JSON.parse(e.data);
+            app.game = JSON.parse(e.data);
         };
     }
 
